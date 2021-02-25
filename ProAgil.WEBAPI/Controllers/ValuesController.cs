bed6c44694d4d61cd4b7bd374e2d1bef8692f22d
vladimirca2000 <vladimirca2000@gmail.com>
2021-02-25 00:28:50 -0300

    Inicial do projeto de eventos

---
+++ ProAgil.WEBAPI/Controllers/ValuesController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WEBAPI.Data;
+using ProAgil.WEBAPI.Model;
+
+namespace ProAgil.WEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        public readonly DataContext Context;
+
+        public ValuesController(DataContext context)
+        {
+            Context = context;
+
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try 
+            {
+                var result = await Context.Eventos.ToListAsync();
+                return Ok(result);
+            }    
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+            
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try 
+            {
+                var result = await Context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                return Ok(result);
+            }    
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+
+            //return Ok(Context.Eventos.ToList().FirstOrDefault(x => x.EventoId == id));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
